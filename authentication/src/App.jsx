cc50dcf21678fc02accba86f7ec74c7456eb66b1
Rakib <rakib@Rakibs-MacBook-Pro.local>
2025-08-11 01:12:45 +0600

    setup firebase

---
+++ authentication/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className='text-center text-2xl text-red-400'>Authentication System</h1>
+    </div>
+  )
+}
---
+++ authentication/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD3zuUkFVfAicEtLcSGt3-HDYhH0I6nfSI",
+  authDomain: "authentication-b2039.firebaseapp.com",
+  projectId: "authentication-b2039",
+  storageBucket: "authentication-b2039.firebasestorage.app",
+  messagingSenderId: "487894182920",
+  appId: "1:487894182920:web:b158d6f5213247abfdf5bd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
