21b7dfc968264b350a20d7480c93f19827a9ab35
Rakib <rakib@Rakibs-MacBook-Pro.local>
2025-08-10 12:11:03 +0600

    keyFrame

--- ReactSuspense/src/App.jsx
+++ ReactSuspense/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Simple from './components/Simple'
+import KeyFrame from './components/KeyFrame'
 
 export default function App() {
   return (
     <div>
-      <Simple />
+      {/* <Simple />
+       */}
+
+       <KeyFrame />
     </div>
   )
 }
---
+++ ReactSuspense/src/components/KeyFrame.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {motion} from 'framer-motion'
+
+export default function KeyFrame() {
+  return (
+    <motion.div 
+    className='bg-blue-500 border w-32 h-32 m-4'
+    animate = {{
+      borderRadius :["20%","20%","50%","50%","20%"],
+      rotate : [0,90,180,270,0],
+      scale : [1,1,2,1,2],
+
+    }}
+     transition  =  {{duration :2}}
+    />
+
+  
+  )
+}
